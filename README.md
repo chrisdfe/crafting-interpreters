daac588531ea720fbd478e4602bcf107b743cb2e
chrisdfe <christopher.d.ferris@gmail.com>
2025-05-12 13:50:40 -0700

    more resolver

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 I'm working through [crafting interpreters](https://craftinginterpreters.com), but writing the interpreter in rust instead of Java. I'm adjusting for my personal tastes as I go, so my language slightly different to the book's language ("Lox")
 
 I am currently here:
-[Chapter 11.2 Resolving & Binding - Semantic Analysis](https://craftinginterpreters.com/resolving-and-binding.html#semantic-analysis)
+[Chapter 11.3.2 Resolving & Binding - resolving variable expressions](https://craftinginterpreters.com/resolving-and-binding.html#resolving-variable-expressions)
--- src/resolver.rs
+++ src/resolver.rs
@@ -1,9 +1,22 @@
 use std::collections::HashMap;
 
-use crate::{interpreter::Interpreter, statements::Stmt};
+use crate::{expressions::Expr, interpreter::Interpreter, statements::Stmt, tokens::Token};
+
+pub struct ResolverErr {
+  pub message: String,
+}
+
+impl ResolverErr {
+  // TODO - token as well
+  pub fn new(message: String) -> Self {
+    Self { message }
+  }
+}
+
+pub type Scope = HashMap<String, bool>;
 
 pub struct Resolver {
-  scopes: Vec<HashMap<String, bool>>,
+  scopes: Vec<Scope>,
 }
 
 impl Resolver {
@@ -24,12 +37,38 @@ impl Resolver {
 
   pub fn resolve_statement(&mut self, statement: &Stmt) {
     //
-    todo!()
+    match statement {
+      Stmt::Var(name, initializer) => {
+        self.declare(name);
+
+        if let Some(initializer) = initializer {
+          self.resolve_expression(initializer);
+        }
+
+        self.define(name);
+      }
+      _ => (),
+    }
   }
 
-  pub fn resolve_expression(&mut self, interpreter: &mut Interpreter) {
-    //
-    todo!()
+  pub fn resolve_expression(&mut self, expression: &Expr) -> Result<(), ResolverErr> {
+    match expression {
+      Expr::Variable(name) => {
+        if let Some(scope) = self.scopes.last() {
+          if let Some(value) = scope.get(&name.lexeme) {
+            if *value == false {
+              return Err(ResolverErr::new(
+                "Can't read local variable in its own initializer.".to_string(),
+              ));
+            }
+          }
+
+          todo!("resolveLocal")
+        }
+        todo!()
+      }
+      _ => Ok(()),
+    }
   }
 
   fn begin_scope(&mut self) {
@@ -38,5 +77,18 @@ impl Resolver {
 
   fn end_scope(&mut self) {
     self.scopes.pop();
+  }
+
+  fn declare(&mut self, name: &Token) {
+    if let Some(scope) = self.scopes.last_mut() {
+      scope.insert(name.lexeme.clone(), false);
+    }
+  }
+
+  fn define(&mut self, name: &Token) {
+    if let Some(scope) = self.scopes.last_mut() {
+      // TODO - dont unwrap, return ResolverErr or whatever
+      *scope.get_mut(&name.lexeme).unwrap() = true;
+    }
   }
 }
